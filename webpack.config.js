c3a1f7ca98d68f0cd3a0d7534c34a3423606f9a8
Yoshihiro Seki <nikochan2k@gmail.com>
2022-03-27 10:51:32 +0900

    commit temporary

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   mode: "development",
   entry: {
-    "basic.spec": "./src/__tests__/basic.spec.ts",
-    "head.spec": "./src/__tests__/head.spec.ts",
+    // "basic.spec": "./src/__tests__/basic.spec.ts",
+    // "head.spec": "./src/__tests__/head.spec.ts",
     "list.spec": "./src/__tests__/list.spec.ts",
   },
   output: {
